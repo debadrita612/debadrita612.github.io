d07bdde40a19e4b779cb36c1b98f8f1dc7741975
Debadrita <debadrita612@gmail.com>
2022-12-23 19:53:04 +0530

    update build for gh-pages

--- src/data.js
+++ src/data.js
@@ -1,35 +1,35 @@
 export const projects = [
     {
         id: 1,
-        img: "https://github.com/debadrita612/weather-app/blob/gh-pages/preview_weather_app.png?raw=true",
+        img: "https://media.istockphoto.com/id/1346481529/photo/sunset-with-orange-clouds-over-the-mountains.jpg?b=1&s=170667a&w=0&k=20&c=BkBm2B5AvxIWIvsa7-D4EdfGVABj3hS7AbvsaN2RtzQ=",
         link: "https://debadrita612.github.io/weather-app/",
         title: "Open Weather App",
         desc: "Tech Stack: HTML, CSS and React JS. Made using OpenWeatherMap API. Get the current weather update of any town/city/country around the world."
     },
     {
         id: 2,
-        img: "https://github.com/debadrita612/Sticky-Notes/blob/main/preview_sticky_notes.png?raw=true",
+        img: "https://images.unsplash.com/photo-1512314889357-e157c22f938d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c3RpY2t5JTIwbm90ZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
         title: "Sticky Notes",
         link: "https://debadrita612.github.io/Sticky-Notes/",
         desc: "Tech Stack: HTML, CCS and Vanilla JS. A sticky notes application with create and delete options. Yes, you may throw all of those paper sticky notes now!"
     },
     {
         id: 3,
-        img: "https://github.com/debadrita612/Digital-Clock/blob/main/preview_digital_clock.png?raw=true",
+        img: "https://images.unsplash.com/photo-1591436123200-5ccc6511c0e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlnaXRhbCUyMGNsb2NrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         title: "Digital Clock",
         link: "https://debadrita612.github.io/Digital-Clock/",
         desc: "Tech Stack: HTML, CSS and Vanilla JS. Basic DOM manipulation. Used an object along with functions that are built in JS to retrieve the time. No additional library required."
     },
     {
         id: 4,
-        img: "https://github.com/debadrita612/Audible/blob/main/preview__audible.png?raw=true",
+        img: "https://images.unsplash.com/photo-1444824775686-4185f172c44b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         title: "Audible",
         link: "https://debadrita612.github.io/Audible/",
         desc: "Tech Stack: HTML, CSS and Vanilla JS. A music player application with all the basic functionalities. Have your own gala time!"
     },
     {
         id: 5,
-        img: "https://github.com/debadrita612/test_repo/blob/master/preview_portfolio.png?raw=true",
+        img: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80",
         title: "Portfolio Website",
         link: "https://debadrita612.github.io/",
         desc: "Tech Stack: HTML, CSS and React JS. My own portfolio website. You can check out my skills and experiences."
